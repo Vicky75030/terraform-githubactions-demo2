cad7e3e8cfe284896aa919407a0e61cc5bc94b66
Vicky75030 <156721841+Vicky75030@users.noreply.github.com>
2024-12-15 19:36:13 -0600

    Update main.tf

--- modules/virtual_machine/main.tf
+++ modules/virtual_machine/main.tf
@@ -10,19 +10,19 @@ resource "azurerm_network_interface" "main" {
     }
 }
 
+resource "azurerm_managed_disk" "os_disk" {
+    name                 = "${var.vm_name}_os_disk"
+    location             = var.location
+    resource_group_name  = var.resource_group_name
+    storage_account_type = "Standard_LRS"
+    create_option        = "FromImage"
+}
 resource "azurerm_virtual_machine" "main" {
     name                  = var.vm_name
     location              = var.location
     resource_group_name   = var.resource_group_name
     network_interface_ids = [azurerm_network_interface.main.id]
     vm_size               = var.vm_size
-
-    storage_os_disk {
-        name              = "${var.vm_name}_os_disk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-    }
 
     storage_image_reference {
         publisher = "Canonical"
