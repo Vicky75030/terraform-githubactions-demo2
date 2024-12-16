a8428949f5a86ce6c20c5d71c95facf3cec1fad1
Vicky75030 <156721841+Vicky75030@users.noreply.github.com>
2024-12-15 19:15:36 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,8 +11,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "SUBSCRIPTION_ID"
-  client_id       = "SERVICE_PRINCIPAL_CLIENT_ID"
+  subscription_id = "d9697186-3cc0-4b84-852b-e4d5827a86cc"
+  client_id       = "ulU8Q~9JBe_QESTxYvFksKnF5gx3V3o0iluuPam8"
   client_secret   = "SERVICE_PRINCIPAL_CLIENT_SECRET"
   tenant_id       = "2c8a943b-68c7-4312-a68c-2c66a9c99eb4"
   
