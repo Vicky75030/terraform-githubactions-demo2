8add7cd5ea4c0dfe10ef059bd90f3496f76e941a
Vicky75030 <156721841+Vicky75030@users.noreply.github.com>
2024-12-15 19:16:50 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
   features {}
 
   subscription_id = "d9697186-3cc0-4b84-852b-e4d5827a86cc"
-  client_id       = "ulU8Q~9JBe_QESTxYvFksKnF5gx3V3o0iluuPam8"
-  client_secret   = "SERVICE_PRINCIPAL_CLIENT_SECRET"
+  client_id       = "ade77c44-528d-40b5-9ad0-35c5d60d1264"
+  client_secret   = "ulU8Q~9JBe_QESTxYvFksKnF5gx3V3o0iluuPam8"
   tenant_id       = "2c8a943b-68c7-4312-a68c-2c66a9c99eb4"
   
 }
